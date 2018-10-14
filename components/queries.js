8a2f0b2556a952d38356bfad48184947c8adbf38
Alex Otten <arswaw@gmail.com>
2018-10-14 18:56:36 -0400

    Layout adjustments

--- components/queries.js
+++ components/queries.js
@@ -1,23 +1,27 @@
-import { APIURL } from '../main.js'
+import {
+    APIURL
+} from '../main.js'
 
 const Queries = {
-    data: {
-        queries: []
+    data: function () {
+        return {
+            queries: []
+        }
     },
     created: async function () {
-        const response = await fetch(`${APIURL}/query`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': "GET"
-            },
-            mode: 'cors',
-        })
+            const response = await fetch(`${APIURL}/query`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Methods': "GET"
+                },
+                mode: 'cors',
+            })
 
-        this.queries = await response.json()
-        console.info("queries from queries.js", this.queries)
-    },
-    template: '#queries-template'
+            this.queries = await response.json()
+            console.info("queries from queries.js", this.queries)
+        },
+        template: '#queries-template'
 }
 
 export default Queries
