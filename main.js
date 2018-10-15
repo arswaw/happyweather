0773673fa7301d842fcbbc0d2d998a9325d3b7e2
Alex Otten <arswaw@gmail.com>
2018-10-14 21:20:22 -0400

    Removed extraneous code from main.js

--- main.js
+++ main.js
@@ -7,75 +7,9 @@ const APIURL = 'http://18.191.127.35:443'
 
 new Vue({
   el: '#app',
-  data: {
-    message: 'Hello Vue!',
-    test: [],
-    place: "",
-    error: "",
-    places: "",
-    showAutocomplete: false,
-    autoCompleteString: "",
-    queries: []
-  },
   components: {
     'queries': Queries,
     'places': Places
-  },
-  methods: {
-    async contactServer() {
-      try {
-        const response = await fetch(`${APIURL}/test`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Methods': "POST"
-          },
-          mode: 'cors',
-          body: JSON.stringify({
-            date: new Date(),
-            city: "South Bend",
-            coords: {
-              "type": "Point",
-              "coordinates": [41.6746991, -86.345924],
-              "state": "IN"
-            }
-          })
-        })
-
-        if (response.status === 200) {
-          this.status = "Sent!"
-        }
-
-      } catch (err) {
-        console.error("Error with retrieving data", err)
-      }
-    },
-    async onPlaceType() {
-      console.info("place", this.place)
-    }
-  },
-  watch: {
-    async place(value) {
-      const response = await fetch(`${APIURL}/text?place=${value}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Methods': "GET"
-        },
-        mode: 'cors',
-      })
-
-      this.places = await response.json()
-
-      if (this.places.status === "ZERO_RESULTS") {
-        this.showAutocomplete = true
-      }
-      if (this.places.status === "OK") {
-
-      }
-
-      console.info("places", this.places)
-    }
   },
   template: '#main-template'
 })
