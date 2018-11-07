fd4982cc2d17365935b0fd7b36270eca4a19d3d1
Alex Otten <arswaw@gmail.com>
2018-11-06 20:09:56 -0500

    Update API IP address
    I changed the EC2 instance.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import Queries from './components/queries.js'
 import Places from './components/places.js'
 
 // Environment Variables
-const APIURL = 'http://18.191.127.35:443'
+const APIURL = 'http://18.222.95.139:3000'
 
 new Vue({
   el: '#app',
