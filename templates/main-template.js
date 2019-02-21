30629ec559faa0941f6e1210c048df2b34d1152b
Alex Otten <arswaw@gmail.com>
2019-02-21 13:45:00 -0500

    Removed extraneous classes and rule properties for grid

--- templates/main-template.js
+++ templates/main-template.js
@@ -1,8 +1,8 @@
 const MainTemplate = /*html*/ `
 <div class="grid-container">
-<aside v-if="isLocalhost" class="app-level-alert">Windchill is dangerously low! Stay inside!</aside>
-<navbar class="grid-column"></navbar>
-<div role="main" class="grid-column">
+<!--<aside v-if="isLocalhost" class="app-level-alert">Windchill is dangerously low! Stay inside!</aside>-->
+<navbar class="grid-item"></navbar>
+<div role="main" class="grid-item">
                 <main role="main" class="router-padding">
                     <transition :appear=true name="slide-fade" mode="out-in">
                         <router-view></router-view>
