f5786f97dbfb37264621327c7fd66ede3c0a7676
arswaw <arswaw@gmail.com>
2019-03-23 00:15:01 -0400

    Changed game state template to use ul

--- templates/character/views/game-state-template.js
+++ templates/character/views/game-state-template.js
@@ -1,13 +1,15 @@
 const GameStateTemplate = /*html*/`
     <div class="box">
         <h4>Game State</h4>
-        Armor Class: {{state.armorClass}}
-        Initiative: {{state.initiative}}
-        Speed: {{state.speed}},
-        Maximum Hit Points: {{state.hitPointMaximum}}
-        Current Hit Points: {{state.currentHitPoints}}
-        Hit Dice: {{state.hitDice}}
-        
+        <ul class="list-group">
+            <li class="list-item">Armor Class: {{state.armorClass}}</li>
+            <li class="list-item">Initiative: {{state.initiative}}</li>
+            <li class="list-item">Speed: {{state.speed}},</li>
+            <li class="list-item">Maximum Hit Points: {{state.hitPointMaximum}}</li>
+            <li class="list-item">Current Hit Points: {{state.currentHitPoints}}</li>
+            <li class="list-item">Hit Dice: {{state.hitDice}}</li>
+        <ul>
+
         <h6>Death Saves</h6>
         Successes: {{state.deathSaves.successes}}
         Failures: {{state.deathSaves.failures}}
