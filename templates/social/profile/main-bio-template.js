c77c96a762a4bb43c8853db850aa02aa72e2b4b6
arswaw <arswaw@gmail.com>
2019-04-19 14:26:35 -0400

    Profile picture css 1.0

--- templates/social/profile/main-bio-template.js
+++ templates/social/profile/main-bio-template.js
@@ -1,5 +1,11 @@
 const MainBioTemplate = /*html*/`
-    <h1>Main Bio Works!</h1>
+    <section>
+        <img src="../../../images/banner.jpg" style="width:100%">
+        <p class="profile-grid">
+            <h2 style="margin-left:50%">Lil Pump</h2>
+            <img src="../../../images/lilpump.jpg" class="profile-photo">
+        </p>
+    </section>
 `
 
 export { MainBioTemplate }
